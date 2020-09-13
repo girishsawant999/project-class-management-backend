6dd43f3c8b1c49e8345b1410246006d8efe47b59
Girish <girishsawant999.gs@gmail.com>
2020-09-13 14:16:07 +0530

    Add restframework in installed apps

--- students/views.py
+++ students/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions
 
 from .models import Student
 from .serializers import StudentSerializer
 
+
 # Create your views here.
+
+
 class StudentViewset(viewsets.ModelViewSet):
-    
     # def get_queryset(self):
     #     user = self.request.query_params.get('user')
     #     queryset = Customer.objects.filter(user=user)
